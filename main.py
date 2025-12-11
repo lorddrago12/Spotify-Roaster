0ecdbb0e25807c0b499b11b3cf8715df132be7fb
Lord Drago <clearrio1465@gmail.com>
2025-12-11 19:03:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBoe5F7wRQKzn00Thp_67o872WnFzWPzeE")
+client = genai.Client(api_key="API_KEY")
 
 prompt = "You are a comedic music critic. Roast the user’s Spotify playlist in a playful, entertaining way. Be witty, sarcastic, and confident, but never mean-spirited. Make sharp jokes about their song choices, genres, and overall music taste while keeping it light-hearted and fun."
 
@@ -10,4 +10,5 @@ response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents= input("Enter Your Spotify Playlist: ") + prompt
 )
+
 print(response.text)
